363fe653909f8a5a644f2b4de9dec269f4cbea5c
Sanjit <sanjitsdev2@gmail.com>
2023-12-27 20:41:45 -0500

    until task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,14 +17,37 @@ const isValid = (username)=>{
     }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{ 
+    let validity = users.filter((user)=>{
+        return(user.username === username && user.password === password)
+    });
+    if(validity.length>0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if(!username || !password){
+    return res.status(404).json({message: "Something went wrong, try again"});
+  }
+  if(authenticatedUser(username,password)){
+    let token = jwt.sign({
+        data:password
+    }, 'access', { expiresIn: 60*60});
+    req.session.authorization = {
+        token,username
+    }
+    return res.status(200).send("Login successful!");
+  }
+  else{
+    return res.status(208).send("Invalid details, please try again");
+  }
 });
 
 // Add a book review
